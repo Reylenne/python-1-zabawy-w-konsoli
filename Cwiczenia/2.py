f7aa085fb0b59c3c7178c3c2d203b9fd6c3bb736
Reylenne <47281148+Reylenne@users.noreply.github.com>
2019-02-02 20:21:21 +0100

    Update 2.py

--- Cwiczenia/2.py
+++ Cwiczenia/2.py
@@ -4,4 +4,9 @@
 kalkulator BMR - Basal metabolic rate
 - na podstawie wzoru BMR dostępnego np. na wikipedii w konsoli oblicz swoje BMR
 """
-
+waga = 63
+wazrost =162
+wiek = 27
+S = -161
+PMM =  10 * waga + 6.25 * wzrost * wiek + S
+print ( " Moje zapotrzebowanie kaloryczne to: " , PMM * 1.2 , "kcal" )
